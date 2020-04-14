c70dcfc32a5a955789a34c65a289c46ed2869a6e
bktotient@gmail.com <bktotient@gmail.com>
2020-04-14 16:14:02 -0400

    add task activity layout

--- app/src/main/java/com/example/hooli/AddTaskActivity.java
+++ app/src/main/java/com/example/hooli/AddTaskActivity.java
@@ -1,4 +1,14 @@
 package com.example.hooli;
 
-public class AddTaskActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task_activity);
+    }
 }
